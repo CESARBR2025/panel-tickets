a8d183f70af3f6e01c7e334df23ea93d60138496
CESARBR2025 <barcenasrosalescesarivan@gmail.com>
2025-12-12 09:37:58 -0600

    no me rindo

--- libs/prisma.ts
+++ libs/prisma.ts
@@ -5,9 +5,8 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
-    log: ['error', 'warn'],
+    // evita keep-alive que rompe pgbouncer
+    datasources: { db: { url: process.env.DATABASE_URL } },
   });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
-
-export default prisma;
